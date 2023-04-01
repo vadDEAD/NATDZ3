7760e1e96d1111d235b3c45558e7d5f90f11b7f9
vaDEAD <t0rm0z.vad.tru@gmail.com>
2023-04-01 18:30:55 +0300

    fix AppVeyor

--- src/test/java/ru/netology/rest/CallBackService.java
+++ src/test/java/ru/netology/rest/CallBackService.java
@@ -7,19 +7,27 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CallBackService {
     private WebDriver driver;
+    private static ChromeOptions options;
+
+
     @BeforeAll
     static  void setUpAll() {
+       ChromeOptions options = new ChromeOptions();
+       options.addArguments("--disable-dev-shm-usage");
+       options.addArguments("--no-sandbox");
+       options.addArguments("--headless");
         System.setProperty("webdriver.chrome.driver", "./drivers/win/chromedriver.exe");
     }
 
     @BeforeEach
     void setUp() {
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
     }
     @AfterEach
     void tearDown(){
@@ -27,21 +35,32 @@ public class CallBackService {
         driver = null;
     }
     @Test
-    void TestV1(){
+    void Test1AllInput(){
         driver.get("http://localhost:9999/");
-        driver.findElements(By.className("input__control")).get(0).sendKeys("текст");
+        driver.findElements(By.className("input__control")).get(0).sendKeys("Текст");
         driver.findElements(By.className("input__control")).get(1).sendKeys("+79998887766");
         driver.findElement(By.className("checkbox")).click();
         driver.findElement(By.className("button__text")).click();
-        String expected = "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        String actual = driver.findElement(By.tagName("p")).getText();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.tagName("p")).getText().trim();
         assertEquals(expected, actual);
     }
 
     @Test
-    void TestWithSpace(){
+    void Test2NameWithSpace(){
         driver.get("http://localhost:9999/");
         driver.findElements(By.className("input__control")).get(0).sendKeys("текст текст");
+        driver.findElements(By.className("input__control")).get(1).sendKeys("+79998887766");
+        driver.findElement(By.className("checkbox")).click();
+        driver.findElement(By.className("button__text")).click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.tagName("p")).getText().trim();
+        assertEquals(expected, actual);
+    }
+    @Test
+    void Test3NameWithHyphen(){
+        driver.get("http://localhost:9999/");
+        driver.findElements(By.className("input__control")).get(0).sendKeys("текст-текст");
         driver.findElements(By.className("input__control")).get(1).sendKeys("+79998887766");
         driver.findElement(By.className("checkbox")).click();
         driver.findElement(By.className("button__text")).click();
