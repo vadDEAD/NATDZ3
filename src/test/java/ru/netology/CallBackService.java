93419ddc9571b3399008166e72d59f2d12499d46
vaDEAD <t0rm0z.vad.tru@gmail.com>
2023-04-01 21:06:09 +0300

    fix AppVeyor

--- src/test/java/ru/netology/CallBackService.java
+++ src/test/java/ru/netology/CallBackService.java
@@ -19,6 +19,7 @@ public class CallBackService {
 
     @BeforeAll
     static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", ".drivers/win/chromedriver.exe/");
         WebDriverManager.chromedriver().setup();
     }
 
