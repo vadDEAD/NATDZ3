32b9c5391ba7b1b2fa39427305919a8406b342c8
vaDEAD <t0rm0z.vad.tru@gmail.com>
2023-04-01 20:55:23 +0300

    fix AppVeyor

--- src/test/java/ru/netology/CallBackService.java
+++ src/test/java/ru/netology/CallBackService.java
@@ -25,6 +25,8 @@ public class CallBackService {
     @BeforeEach
     public void beforeEach() {
         ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
     }
